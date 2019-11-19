d7de7e9d88ffb258a8aa753d85827e90e7f4d40e
scottrobel <robelrain@gmail.com>
2019-11-19 00:11:00 -0500

    make the code comply with ruby style guide

--- lib/position.rb
+++ lib/position.rb
@@ -12,7 +12,7 @@ class Position
   end
 
   %w[top bottom right left].each do |direction|
-    define_method("#{direction}") do
+    define_method(direction.to_s) do
       position = neighbor_position(direction)
       Position.new(position)
     end
@@ -22,8 +22,8 @@ class Position
     %w[right left].each do |horizontal_direction|
       method_name = "#{vertical_direction}_#{horizontal_direction}"
       define_method(method_name) do
-        shift_one = method("#{vertical_direction}".to_sym).call
-        shift_one.method("#{horizontal_direction}".to_sym).call
+        shift_one = method(vertical_direction.to_s.to_sym).call
+        shift_one.method(horizontal_direction.to_s.to_sym).call
       end
     end
   end
@@ -32,19 +32,13 @@ class Position
 
   def neighbor_position(direction)
     position = Marshal.load(Marshal.dump(@position))
-    case direction
-    when 'top'
-      position[1] += 1
-    when 'bottom'
-      position[1] -= 1
-    when 'right'
-      position[0] += 1
-    when 'left'
-      position[0] -= 1
-    end
+    position[1] += 1 if direction == 'top'
+    position[1] -= 1 if direction == 'bottom'
+    position[0] += 1 if direction == 'right'
+    position[0] -= 1 if direction == 'left'
     position
   end
 end
-position = Position.new([1,1])
+position = Position.new([1, 1])
 tr = position.top_right
 binding.pry
