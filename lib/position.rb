fa3d98602d8ea58a1feb3e3e97c75bdd44635c1a
scottrobel <robelrain@gmail.com>
2019-11-18 21:04:39 -0500

    add neighbor_position method to Position class that return the position of a square in a direction

--- lib/position.rb
+++ lib/position.rb
@@ -8,4 +8,20 @@ class Position
   def to_s
     @position
   end
+
+  private
+
+  def neighbor_position(direction)
+    position = Marshal.load(Marshal.dump(@position))
+    case direction
+    when 'top'
+      position[1] += 1
+    when 'bottom'
+      position[1] -= 1
+    when 'right'
+      position[0] += 1
+    when 'left'
+      position[0] -= 1
+    end
+  end
 end
