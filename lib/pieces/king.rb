b2cff443d60fca3b75ca202cd648047ee8e173ff
scottrobel <robelrain@gmail.com>
2019-11-19 17:42:43 -0500

    add king class

---
+++ lib/pieces/king.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# can move in any direction 1 move
+# can not move into check
+class King < Piece
+  BEHAVIORS = [
+    {
+      start_condition: 'none',
+      iterations: 1,
+      directions:
+        %w[top
+           bottom
+           right
+           left
+           top_right
+           top_left
+           bottom_right
+           bottom_left],
+      take_oponent: true,
+      break_before: 'opponent_moves'
+    }
+  ].freeze
+end
