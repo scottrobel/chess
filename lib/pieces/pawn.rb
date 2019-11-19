9c56dab429d9af9669e90e2f82717131330208ed
scottrobel <robelrain@gmail.com>
2019-11-19 17:24:13 -0500

    add pawn class

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,17 +3,27 @@
 # can up one or two on first move
 # can move one up any other move
 # can only take oponent Pieces Diagonally
-class Pawn
-  attr_reader :position, :color, :behaviors
-  def initialize(position, color)
-    @position = Position.new(position)
-    @color = color
-    @behaviors = [
+class Pawn < Piece
+  BEHAVIORS = [
       { start_condition: 'front_line',
-        iterations: 1,
+        iterations: 2,
         directions: %w[top],
         take_oponent: false,
-        break_before: 'own_piece' }
-    ]
-  end
+        break_before: 'own_piece' 
+      },
+      {
+        start_condition: 'opponent_on top_right',
+        iterations: 1,
+        directions: %w[top_right],
+        take_oponent: true,
+        break_before: 'own_piece'
+      },
+      {
+        start_condition: 'opponent_on top_left',
+        iterations: 1,
+        directions: %w[top_left],
+        take_oponent: true,
+        break_before: 'own_piece'
+      }
+    ].freeze
 end
