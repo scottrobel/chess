bf41aa2f5e8e7e000e2c5f21a6bfa533fcf8c5bb
scottrobel <robelrain@gmail.com>
2019-11-19 20:20:24 -0500

    add set_up_pawns test spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,25 @@
-require './lib/board'
+require 'colorized_string'
+require 'pry'
+require './lib/position'
+require './lib/pieces/piece'
 require './lib/pieces/pawn'
+require './lib/board'
 
+describe Board do
+  before(:each) do
+    @new_board = Board.new
+  end
+  describe '#set_up_pawns' do
+    before(:each) do
+      @new_board.set_up_pawns
+    end
+    it 'sets up pawns' do
+      @new_board.game_board[1].each do |pawn|
+        expect(pawn.class).to eql(Pawn)
+      end
+      @new_board.game_board[6].each do |pawn|
+        expect(pawn.class).to eql(Pawn)
+      end
+    end
+  end
+end
